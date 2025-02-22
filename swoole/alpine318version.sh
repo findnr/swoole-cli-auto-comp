fd6f2d1f7aedb6cc6ad32cdb05ebc80983277cbb
findnr <cym504875043@gmail.com>
2025-02-22 06:58:09 +0000

    Create alpine318version.sh

---
+++ swoole/alpine318version.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+# 定义基础镜像和容器名称
+BASE_IMAGE="alpine:3.18"
+CONTAINER_NAME="swoole-cli-main318"
+WORK_DIR="/mnt/${CONTAINER_NAME}"
+
+# 检查并克隆仓库
+if [ ! -d "$WORK_DIR" ]; then
+  echo "克隆 swoole-cli 仓库到 $WORK_DIR..."
+  git clone https://github.com/swoole/swoole-cli.git $WORK_DIR
+  cd ext/swoole/
+  git checkout v5.1.7.0
+  cd $WORK_DIR
+  git submodule update --init
+else
+  sudo rm -rf $WORK_DIR
+  git clone https://github.com/swoole/swoole-cli.git $WORK_DIR
+  cd ext/swoole/
+  git checkout v5.1.7.0
+  cd $WORK_DIR
+  git submodule update --init
+fi
+
+# 进入工作目录并初始化环境
+cd $WORK_DIR
+bash setup-php-runtime.sh
+composer install
+php prepare.php
+php prepare.php +inotify +mongodb +xlswriter
+
+# 定义在容器内执行的命令
+CONTAINER_COMMANDS="
+cd /work &&
+sh setup-php-runtime.sh &&
+export PATH=\$PATH:/work/bin/runtime &&
+sh sapi/quickstart/linux/alpine-init.sh &&
+php prepare.php &&
+php prepare.php +inotify +mongodb +xlswriter &&
+./make.sh all-library &&
+./make.sh config &&
+./make.sh build &&
+./make.sh archive
+exit
+"
+# 检查容器是否存在
+if docker ps -a --format "{{.Names}}" | grep -q "^${CONTAINER_NAME}$"; then
+  echo "容器 ${CONTAINER_NAME} 已存在，直接进入执行命令..."
+  script -q -c "docker exec -it $CONTAINER_NAME /bin/sh -c '$CONTAINER_COMMANDS'"
+else
+  echo "容器 ${CONTAINER_NAME} 不存在，创建新容器并执行命令..."
+  docker pull $BASE_IMAGE
+  docker run --tty=false -dit --name $CONTAINER_NAME -v $WORK_DIR:/work $BASE_IMAGE /bin/sh
+  script -q -c "docker exec -it $CONTAINER_NAME /bin/sh -c '$CONTAINER_COMMANDS'"
+
+fi
